90e41ae31b54a9afff2abf6adb7659812c6ffeb3
clement-wds <clementwalshdeserrant@outlook.com>
2021-11-15 17:41:46 +0100

    Model Profile

---
+++ lib/model/profile.dart
@@ -0,0 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Profile{
+  String name = "";
+  String lastname = "";
+  String image= "";
+  DateTime birthday = DateTime.now();
+
+  Profile(DocumentSnapshot snapshot){
+    String uid = snapshot.id;
+    Map<String, dynamic> map = snapshot.data() as Map<String, dynamic>;
+    name = map["PRENOM"];
+    lastname = map["NOM"];
+    image = map["IMAGE"];
+
+    Timestamp time = map["BIRHTDAY"];
+    birthday = time.toDate();
+  }
+}
